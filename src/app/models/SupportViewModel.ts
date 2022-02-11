7fc444d68b826a921a821e31030fe49c5dae187e
Michael Keeney-Robinson <mkeeney-robinson@lcptracker.com>
2022-02-10 17:57:15 -0800

    tinkering more

--- src/app/models/SupportViewModel.ts
+++ src/app/models/SupportViewModel.ts
@@ -3,38 +3,38 @@ import { Guid } from "guid-typescript";
 ///
 //< Work in Progress> still
 ///
-export interface SupportViewModel {
-  SearchKey?: string;
-  BtnAction: string;
-  RecsPerPage: number;
-  PageNo: number;
-  Orderby: string;
-  IsAssending: boolean;
+export class SupportViewModel {
+  SearchKey?: string = "";
+  BtnAction: string = "";
+  RecsPerPage: number = 10;
+  PageNo: number = 1;
+  Orderby: string = "";
+  IsAssending: boolean = false;
   ErrorMessage?: string;
   RecordCount?: number;
-  Users: Array<UserRecordVM>;
+  Users!: Array<UserRecordVM>;
 }
 
-export interface UserRecordVM {
+export class UserRecordVM {
   Id?: Guid;
-  UserName: string;
-  Show: boolean;
-  Name: string;
-  Email: string;
-  EmailValidated: boolean;
-  Phone: string;
-  Company: string;
-  Country: string;
-  TfaIsEnabled: string;
-  History: Array<HistoryRec>;
+  UserName: string = "";
+  Show: boolean = false;
+  Name: string = "";
+  Email: string = "";
+  EmailValidated: boolean = false;
+  Phone: string = "";
+  Company: string = "";
+  Country: string = "";
+  TfaIsEnabled: string = "";
+  History: Array<HistoryRec> = new Array<HistoryRec>();
 
 }
 
 
-export interface HistoryRec {
-  Event: Date;
-  GatewayIPAddress: string;
-  IsAuthenticated: boolean;
+export class HistoryRec {
+  Event: Date = new Date();
+  GatewayIPAddress: string = "";
+  IsAuthenticated: boolean = false;
 }
 
 
--- src/app/services/user-manager-services.ts
+++ src/app/services/user-manager-services.ts
@@ -9,9 +9,9 @@ import { SupportViewModel, UserRecordVM } from '../models/SupportViewModel';
 })
 export class UserServices {
     userList: Array<SupportViewModel> = ([
-        { Users: Array<UserRecordVM>(), SearchKey: '', BtnAction: '', RecsPerPage: 10, PageNo: 1, Orderby: 'name', IsAssending: true, ErrorMessage: '', RecordCount: 0 },
-
+        // { Users: [{ Id: 83F7D262-04DB-40D6-B585-164C6205C508, UserName: 'ADMIN@JUSTDELETE.ME', Email: "ADMIN@JUSTDELETE.ME", EmailConfirmed: true, PhoneNumber: "0000000000", TwoFactorEnabled: false, LockoutEnabled: false, AccessFailedCount: 0, FirstName: "MONKEY", LastName: "ADMIN@JUSTDELETE.ME", PrimaryIdentifierNumber: "12121212", PhoneExtension: "", DateUpdated: "2021-02-24T17:26:44.4460150", Orgs: [{ OrgCode: "qalcpv2", OrgName: "QA LCP V2", OrgStatus: "active", OrgType: 0 }], Logins: [{ Event: "2021-04-12T16:37:20.8419867", IsAuthenticated: true, GatewayIPAddress: "71.80.171.173" }, { Event: "2021-04-06T16:50:05.1425793", IsAuthenticated: true, GatewayIPAddress: "::1" }}]},
     ]);
+
 
     constructor() { }
     get() {
